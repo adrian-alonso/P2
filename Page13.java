77f01f7ed7a9a2d7a992e6532cd5d1566f054aec
Adrián Alonso <adrianalonsovilar@gmail.com>
2020-11-03 22:49:35 +0100

    v2

--- Page13.java
+++ Page13.java
@@ -4,8 +4,8 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class Page01 extends Sint101P2 {
-  public void phase01 (HttpServletRequest request, HttpServletResponse response) {
+public class Page13 extends Sint101P2 {
+  public void phase13 (HttpServletRequest request, HttpServletResponse response, String pdegree, String psubject) {
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
 
@@ -18,12 +18,8 @@ public class Page01 extends Sint101P2 {
     out.println("<h1>Servicio de consulta de expedientes acad&eacutemicos</h1>");
     out.println("</header>");
     out.println("<section>");
-    out.println("<h2>Bienvenido a este servicio</h2>");
-    out.println("<p>Ver los ficheros err&oacuteneos</p>");
-    out.println("</section>");
-    out.println("<section>");
-    out.println("<h3>Selecciona una consulta:</h3>");
-    out.println("<p>Consulta 1: Ver los alumnos de una asignatura de una titulaci&oacuten</p>");
+    out.println("<h2>Consulta 1: Fase 3 (Titulaci&oacuten=" + pdegree + ", Asignatura=" + psubject + ")</h2>");
+    out.println("<p>Este es el resultado:</p>");
     out.println("</section>");
     out.println("<footer>");
     out.println("<p>&copy Adri&aacuten Alonso Vilar (2020-2021)</p>");
--- Sint101P2.java
+++ Sint101P2.java
@@ -17,55 +17,44 @@ public class Sint101P2 extends HttpServlet {
   public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     String pphase = request.getParameter("pphase");
     String passwd = request.getParameter("p");
+    String pdregree = request.getParameter("pdegree");
+    String psubject = request.getParameter("psubject");
+    Page01 page01 = new Page01();
+    Page02 page02 = new Page02();
+    Page11 page11 = new Page11();
+    Page12 page12 = new Page12();
+    Page13 page13 = new Page13();
 
     if (pphase == null) {
-      this.phase01(request, response);
+      page01.phase01(request, response);
     } else {
+      switch (pphase.trim()) {
+        case "01":
+          page01.phase01(request, response);
+          break;
 
+       case "02":
+         page02.phase02(request, response);
+         break;
+
+       case "11":
+         page11.phase11(request, response);
+         break;
+
+       case "12":
+         page12.phase12(request, response, pdegree);
+         break;
+
+       case "13":
+         page13.phase13(request, response, pdegree, psubject);
+         break;
+
+       default:
+         page01.phase01(request, response);
+         break;
+
+      }
     }
-
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-    // out.println("");
-
   }
 
-  public void phase01 (HttpServletRequest request, HttpServletResponse response) {
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>P2EA</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<header>");
-    out.println("<h1>Servicio de consulta de expedientes acad&eacutemicos</h1>");
-    out.println("</header>");
-    out.println("<section>");
-    out.println("<h2>Bienvenido a este servicio</h2>");
-    out.println("<p>Bienvenido a este servicio</p>");
-    out.println("</section>");
-    out.println("<section>");
-    out.println("<h3>Selecciona una consulta:</h3>");
-    out.println("<p>Consulta 1: Ver los alumnos de una asignatura de una titulaci&oacuten</p>");
-    out.println("</section>");
-    out.println("<footer>");
-    out.println("<p>&copy Adri&aacuten Alonso Vilar (2020-2021)</p>");
-    out.println("</footer>");
-    out.println("</body>");
-    out.println("</html>");
-  }
 }
